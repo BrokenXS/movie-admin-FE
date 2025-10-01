b93cc6cb5cabb82dad7d3291054fd71482351125
BrokenXS <toannguyen200044@gmail.com>
2025-10-02 00:34:51 +0700

    relocate git

--- movie-admin-be/movie-management/models/movie.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class MovieModel(BaseModel):
-    id: Optional[int]
-    title: str
-    description: Optional[str] = None
-    year: Optional[int] = None
